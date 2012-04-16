67eb504077f0b6541a2ecdf875aa6ad4d425fc5b
Dennis Schubert <software@dsx.cc>
2012-04-16 21:29:18 +0200

    Here you are, world.

---
+++ README.md
@@ -0,0 +1,57 @@
+HTML5 Soundboard
+================
+
+A damn small soundboard based on pure HTML5 (and some JavaScript)
+
+Setup
+-----
+
+1. Upload the files
+2. Place sound files (preferably .ogg's) in the /assets/sounds/-directory
+3. Modify /assets/settings/sounds.json to contain all your sounds
+4. Modify /assets/settings/general.json to match your favorite colors
+5. Have fun clicking around
+
+sounds.json
+-----------
+
+That file is basically just a json file containing an array of sounds. It should look like that:
+
+```json
+[
+    {
+        "title": "My awesome sound",
+        "file": "sound.ogg"
+    },
+    {
+        "title": "Another sound",
+        "file": "foo.ogg"
+    }
+]
+```
+
+settings.json
+-------------
+
+That file is json, too. Use it to change the colors of the soundboard.
+
+```json
+{
+    "colors":
+    {
+        "background": "#615A55",
+        "button": "#DEDEDE",
+        "playing": "#EA2C46"
+    }
+}
+```
+
+Bug? Feature idea?
+------------------
+
+Feel free to fork, I'd be glad to merge a pull request. ;-)
+
+Notes
+-------
+
+I am using jQuery, Handlebars and normalize.css. Note that that stuff is not licensed under the DWTFYWTADBUPL.
---
+++ assets/scripts/main.js
@@ -0,0 +1,97 @@
+jQuery(document).ready(function() {
+    loadAndApplySettings();
+    loadAudioButtons();
+});
+
+jQuery(window).resize(function () {
+    centerTheBoard();
+});
+
+function loadAndApplySettings() {
+    jQuery.getJSON("assets/settings/general.json", function(settings) {  
+        var source = jQuery("#tpl-color-styles").html();
+        var template = Handlebars.compile(source);
+        var context = {"colors": settings["colors"]}
+        var styleRules = template(context);
+        
+        jQuery("head").append(styleRules);
+    });
+}
+
+function loadAudioButtons() {
+    jQuery.getJSON("assets/settings/sounds.json", function(sounds) {
+        var source = jQuery("#tpl-buttons").html();
+        var template = Handlebars.compile(source);
+        var context = {"sounds": sounds}
+        var soundboard = template(context);
+        
+        jQuery("#soundboard").html(soundboard);
+        
+        handleAudioButtonClicks();
+        centerTheBoard();
+        openTheCurtain();
+    });
+}
+
+function handleAudioButtonClicks() {
+    jQuery("audio").each(function (i, elem) {
+        elem.addEventListener('playing', function(){
+            jQuery(this).parent().addClass("playing");
+        }, false); 
+        
+        elem.addEventListener('pause', function(){
+            jQuery(this).parent().removeClass("playing");
+        }, false);
+        
+        elem.addEventListener('ended', function(){
+            jQuery(this).parent().removeClass("playing");
+        }, false);
+    });
+    
+    jQuery(".button").click(function () {
+        var isPlaying = jQuery(this).hasClass("playing");
+        var audioElement = jQuery(this).find("audio").get(0);
+        
+        if (!isPlaying) {
+            audioElement.play();
+        } else {
+            audioElement.pause();
+            audioElement.currentTime = 0.0;
+        }
+    });
+}
+
+function centerTheBoard() {
+    var sb = jQuery("#soundboard");
+    
+    var numBoxes = jQuery(".button").length;
+    var screenRes = jQuery(document).width() / jQuery(document).height();
+    
+    var x = 1, y = 1;
+    
+    while ((x*y) < numBoxes) {
+        var solRes = x / y;
+        
+        if (solRes < screenRes) {
+            x++;
+        } else {
+            y++;
+        }
+    }
+    
+    var boxDimension = jQuery(".button").first().width() + 30;
+    
+    sb.width(boxDimension * x);
+    sb.height(boxDimension * y);
+    
+    sb.css("margin-left", "-" + sb.width()/2 + "px");
+    sb.css("margin-top", "-" + sb.height()/2 + "px");
+}
+
+function openTheCurtain() {
+    var fadeDuration = 200;
+    
+    jQuery("#loader").fadeOut(fadeDuration, function() {
+        jQuery("#soundboard").fadeIn(fadeDuration);
+    });
+}
