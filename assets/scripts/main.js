c56315d91fdc8f33f0501c460aa680cacd05b45c
Dennis Schubert <software@dsx.cc>
2014-12-02 04:26:15 +0100

    Ditch jQuery for DomContentLoading and resize-handling

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,11 @@
-jQuery(document).ready(function() {
+/* vim: set tw=4 sw=4: */
+
+document.addEventListener('DOMContentLoaded', function(){
     loadAndApplySettings();
     loadAudioButtons();
 });
 
-jQuery(window).resize(function () {
+window.addEventListener("resize", function() {
     centerTheBoard();
 });
 
@@ -13,7 +15,7 @@ function loadAndApplySettings() {
         var template = Handlebars.compile(source);
         var context = {"colors": settings["colors"]}
         var styleRules = template(context);
-        
+
         jQuery("head").append(styleRules);
     });
 }
@@ -24,9 +26,9 @@ function loadAudioButtons() {
         var template = Handlebars.compile(source);
         var context = {"sounds": sounds}
         var soundboard = template(context);
-        
+
         jQuery("#soundboard").html(soundboard);
-        
+
         handleAudioButtonClicks();
         centerTheBoard();
         openTheCurtain();
@@ -37,21 +39,21 @@ function handleAudioButtonClicks() {
     jQuery("audio").each(function (i, elem) {
         elem.addEventListener('playing', function(){
             jQuery(this).parent().addClass("playing");
-        }, false); 
-        
+        }, false);
+
         elem.addEventListener('pause', function(){
             jQuery(this).parent().removeClass("playing");
         }, false);
-        
+
         elem.addEventListener('ended', function(){
             jQuery(this).parent().removeClass("playing");
         }, false);
     });
-    
+
     jQuery(".button").click(function () {
         var isPlaying = jQuery(this).hasClass("playing");
         var audioElement = jQuery(this).find("audio").get(0);
-        
+
         if (!isPlaying) {
             audioElement.play();
         } else {
@@ -63,12 +65,12 @@ function handleAudioButtonClicks() {
 
 function centerTheBoard() {
     var sb = jQuery("#soundboard");
-    
+
     var numBoxes = jQuery(".button").length;
     var screenRes = jQuery(document).width() / jQuery(document).height();
-    
+
     var x = 1, y = 1;
-    
+
     /**
      * this super-hyper-high-performance loop is developed
      * and powered by @SWW13
@@ -76,26 +78,26 @@ function centerTheBoard() {
      */
     while ((x*y) < numBoxes) {
         var solRes = x / y;
-        
+
         if (solRes < screenRes) {
             x++;
         } else {
             y++;
         }
     }
-    
+
     var boxDimension = jQuery(".button").first().width() + 30;
-    
+
     sb.width(boxDimension * x);
     sb.height(boxDimension * y);
-    
+
     sb.css("margin-left", "-" + sb.width()/2 + "px");
     sb.css("margin-top", "-" + sb.height()/2 + "px");
 }
 
 function openTheCurtain() {
     var fadeDuration = 200;
-    
+
     jQuery("#loader").fadeOut(fadeDuration, function() {
         jQuery("#soundboard").fadeIn(fadeDuration);
     });
